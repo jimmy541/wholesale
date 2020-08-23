60fcc6f6b1da64b54a0cf2f8f7fa35ae15659fa9
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-22 22:38:30 -0700

    Update process-registration.php
    Send activation link to new registration

--- php-scripts/process-registration.php
+++ php-scripts/process-registration.php
@@ -1,7 +1,13 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 $preventLoginPageLoop = 'fd5as431';
 require($_SERVER['DOCUMENT_ROOT'].'/wholesale/include/connect.php');
-//require_once('mail.php');
+
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
 
 if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['company_name'])){
 	if (!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['company_name'])){
@@ -67,20 +73,40 @@ if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['e
 					$stmt->execute();
 					$stmt->close();
 				}
-				/*
-				//send activation code
-				$mail->addAddress($email);     // Add a recipient
-				$mail->Subject = 'Activate New Account at 7awel.com';
+				/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+				$mail = new PHPMailer(TRUE);
 
-				$message = '<!DOCTYPE html><html dir="rtl" lang="ar"><head><meta charset="utf-8"><title>Activate new account</title><style>body{font-size: 20px;line-height: 30px;}h1{color: #007dc6;}</style></head><body><h1 style="direction:rtl">&#1605;&#1585;&#1581;&#1576;&#1575;&#1611; &#1576;&#1603;&#1605; &#1601;&#1610; &#1605;&#1608;&#1602;&#1593; &#1581;&#1575;&#1608;&#1604;.</h1>';
-				$message .= '<div style="direction:rtl">&#1604;&#1602;&#1583; &#1602;&#1605;&#1578;&#1605; &#1576;&#1578;&#1587;&#1580;&#1610;&#1604; &#1581;&#1587;&#1575;&#1576; &#1580;&#1583;&#1610;&#1583; &#1601;&#1610; &#1575;&#1604;&#1605;&#1608;&#1602;&#1593;. &#1606;&#1608;&#1583; &#1576;&#1588;&#1603;&#1585;&#1603;&#1605; &#1593;&#1604;&#1609; &#1575;&#1604;&#1573;&#1606;&#1590;&#1605;&#1575;&#1605; &#1604;&#1604;&#1605;&#1608;&#1602;&#1593; &#1608;&#1606;&#1578;&#1605;&#1606;&#1609; &#1604;&#1603;&#1605; &#1571;&#1608;&#1602;&#1575;&#1578; &#1605;&#1605;&#1578;&#1593;&#1577;.<br />';
-				$message .= ' &#1571;&#1606;&#1578;&#1605; &#1593;&#1604;&#1609; &#1576;&#1593;&#1583; &#1582;&#1591;&#1608;&#1577; &#1608;&#1575;&#1581;&#1583;&#1577; &#1605;&#1606; &#1578;&#1601;&#1593;&#1610;&#1604; &#1581;&#1587;&#1575;&#1576;&#1603;&#1605;  &#1601;&#1610; &#1581;&#1575;&#1608;&#1604;.<br />';
-				$message .= '&#1575;&#1604;&#1585;&#1580;&#1575;&#1569; &#1575;&#1604;&#1602;&#1610;&#1575;&#1605; &#1576;&#1575;&#1604;&#1590;&#1594;&#1591; &#1593;&#1604;&#1609; &#1575;&#1604;&#1585;&#1575;&#1576;&#1591; &#1575;&#1604;&#1570;&#1578;&#1610; &#1603;&#1610; &#1578;&#1578;&#1605;&#1603;&#1606;&#1608;&#1575; &#1605;&#1606; &#1578;&#1601;&#1593;&#1610;&#1604; &#1581;&#1587;&#1575;&#1576;&#1603;&#1605; <a href="http://7awel.com/activate?email='.$email.'&id='.$activationCode.'">'.'&#1573;&#1590;&#1594;&#1591; &#1607;&#1606;&#1575;</a></div></body></html>';
+				/* Open the try/catch block. */
+				try {
+				   /* Set the mail sender. */
+				   $mail->setFrom('registration@dalysoft.com', 'WholeSale');
 
-				$mail->Body    = $message;
-				
-				$mail->Send();
-				*/
+				   /* Add a recipient. */
+				   $mail->addAddress($email, 'Dalysoft');
+
+				   /* Set the subject. */
+				   $mail->Subject = "Account Acivation";
+
+				   /* Set the mail message body. */
+				   $mail->Body = "Welcome to Dalysoft. Please activate your account using the following link.";
+					
+					// Attachments
+					
+					
+				   /* Finally send the mail. */
+				   $mail->send();
+				   
+				}
+				catch (Exception $e)
+				{
+				   /* PHPMailer exception. */
+				   echo $e->errorMessage();
+				}
+				catch (\Exception $e)
+				{
+				   /* PHP exception (note the backslash to select the global namespace Exception class). */
+				   echo $e->getMessage();
+				}
 				
 				
 				$_SESSION['user'] = $email;
