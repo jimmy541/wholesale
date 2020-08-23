ac86d54d97fa35ea7001a778ea25c8c1689f5669
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-22 20:58:28 -0700

    Update accounts-history.php

--- accounts-history.php
+++ accounts-history.php
@@ -124,7 +124,7 @@ function validateDate($date, $format = 'Y-m-d')
 
 
 
-
+<input type="hidden"></input>
 <input type="hidden" id="customerhash" value="<?php echo $hashed_customer_number; ?>"/>
 <input type="hidden" id="datefrom" value="<?php echo $datefrom; ?>"/>
 <input type="hidden" id="dateto" value="<?php echo $dateto; ?>"/>
