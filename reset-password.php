f6c4f2d609c7255ea998d38892e84ba093cd773c
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-24 15:18:01 -0700

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -31,12 +31,13 @@ if (isset($_GET['status'])){
 	
 	<form method="post" action="php-scripts/send-reset-password-link.php" class="form-signin">
 	<h1 class="h3 mb-3 font-weight-normal">Reset Password</h1>
-	<span>Enter the you used to register with Wholesale and we will send you instructions to reset your password.</span>	
+	<span>Enter the you used to register with Wholesale and we will send you instructions to reset your password.</span><br>
 	<label class="sr-only" for="email">* Email Address</label>
 	<input class="form-control" type="text" name="email" id="email" placeholder="* Email Address" autocomplete="off" /><br>
 	
 	<!--<div class="g-recaptcha" data-sitekey="6LedkEgUAAAAAAwqNg6VHY9ei5T8VjKRD29xl3w5"></div>-->
 	<button class="btn btn-lg btn-primary btn-block" id="resetbtn" type="submit">Submit</button>
+	<span><a href="login.php" style="text-decoration:none;">Back to Login</a></span>
 	</form>
 </div>
 
