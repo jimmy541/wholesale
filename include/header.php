316d1d57be18be182af85e1568ab25ec83f2fde6
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-25 19:08:20 -0700

    Update header.php

--- include/header.php
+++ include/header.php
@@ -10,7 +10,7 @@
 <link rel="shortcut icon" href="favicon.ico"/>
 <?php/* echo $page_meta; */?>
 <title><?php echo $page_title; ?></title>
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/wholesale/css/style.css';?>">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/css/uikit.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit.min.js"></script>
@@ -18,9 +18,9 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="//malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.min.css">
-<link rel="stylesheet" href="css/sidebar-themes.css">
-<script type="text/javascript" src="js/dashboard-page.js"></script>
-<link rel="stylesheet" type="text/css" href="css/populateContainers.css">
+<link rel="stylesheet" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/wholesale/css/sidebar-themes.css';?>">
+<script type="text/javascript" src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/wholesalejs/dashboard-page.js';?>"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $_SERVER['DOCUMENT_ROOT'].'/wholesale/css/populateContainers.css';?>">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
