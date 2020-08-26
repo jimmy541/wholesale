4fdd8c7b31f363368efa2e78f8291ede3419d97b
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-25 22:41:49 -0700

    Create clients.php

---
+++ backend/clients.php
@@ -0,0 +1,57 @@
+<?php $page_title = 'Backend Clients';
+$more_script = '<script type="text/javascript" src="../js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" href="../css/jquery.dataTables.min.css">';
+?>
+<table class="row-border" id="gtable">
+<caption>Products New</caption>
+	<thead>
+		<tr>
+			<th>Client ID</th>
+			<th>Name</th>
+			<th>City</th>
+			<th>State</th>
+			<th>Phone</th>
+			<th>Created</th>
+			
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		$query = "SELECT * FROM `clients` ORDER BY `date_created` DESC";
+		
+		$result = mysqli_query($link, $query); 
+		while($row = mysqli_fetch_array($result)) { 
+			echo '<tr><td data-label="Client Id">'.htmlspecialchars($row["clientid"]).'</td>
+			<td data-label="Company Name">'.htmlspecialchars($row["company_name"]).'</td>
+			<td data-label="City">'.htmlspecialchars($row["city"]).'</td>
+			<td data-label="State">'.htmlspecialchars($row['state']).'</td>
+			<td data-label="Phone">'.htmlspecialchars($row['phone1']).'</td>
+			<td data-label="Created">'.htmlspecialchars($row['date_created']).'</td>';
+		}
+		?>
+</tbody>
+</table>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#gtable').DataTable({
+		columnDefs: [
+			{
+				targets: -1,
+				className: 'dt-left'
+			}
+		]
+	});
+	
+});
+</script>
+<!-- The following div closes the main body div -->
+</div>
+
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/wholesale/include/header.php"); ?>
+
+
+
+
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/wholesale/include/footer.php"); ?>
