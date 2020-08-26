c120ac8ca13c8f42c88f21934a4fe7593725e884
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-25 19:32:50 -0700

    Update main.php

--- backend/main.php
+++ backend/main.php
@@ -16,44 +16,8 @@
       
     </div></a>
   </div>
-  <div class="card text-center">
-    <a href="list-customers.php"><div class="card-body">
-	<h1 class="card-title"><i class="fa fa-user-friends"></i></h1>
-      <h5 class="card-title">Customers</h5>
-      
-    </div></a>
-  </div>
-  <div class="card text-center">
-    <a href="list-users.php"><div class="card-body">
-	<h1 class="card-title"><i class="fa fas fa-user"></i></h1>
-      <h5 class="card-title">Users</h5>
-      
-    </div></a>
-  </div>
-</div><br>
-<div class="card-deck">
-  <div class="card text-center">
-   <a href="list-orders.php"><div class="card-body">
-	<h1 class="card-title"><i class="fa fa-list-alt"></i></h1>
-      <h5 class="card-title">Invoices</h5>
-      
-    </div></a>
-  </div>
-  <div class="card text-center">
-    
-    <a href="#" id="place-order-template1-2"><div class="card-body">
-	<h1 class="card-title"><i class="fa fa-pen-square"></i></h1>
-      <h5 class="card-title">Place Order</h5>
-      
-    </div></a>
-  </div>
-  <div class="card text-center">
-    <a href="edit-settings.php"><div class="card-body">
-	<h1 class="card-title"><i class="fa fa-user-friends"></i></h1>
-      <h5 class="card-title">Settings</h5>
-      
-    </div></a>
-  </div>
+ 
+
   
 </div>
 
