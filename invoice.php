ced85397ddb0d604bafad92ae2234fd9288503df
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-30 15:38:29 -0700

    Update invoice.php

--- invoice.php
+++ invoice.php
@@ -214,9 +214,9 @@ if(isset($_GET['invoice']) && !empty($_GET['invoice'])){
 						$x++;
 						echo '<tr>';
 						echo '<td>'.htmlspecialchars($ce_co).'</td>';
-						echo '<td><input type="text" value="'.htmlspecialchars($des).' '.$pk.'x'.$sz.'" id="desc" style="width:310px; id="desc'.$id.'"/></td>';
-						echo '<td><input type="number" step=".01" value="'.htmlspecialchars($qy).'" id="qty" style="width:50px;" id="qty'.$id.'"/></td>';
-						echo '<td align="right"><input type="text" value="'.htmlspecialchars($ret).'" id="retail" style="max-width:90px;" id="ret'.$id.'"/></td>';
+						echo '<td><input type="text" value="'.htmlspecialchars($des).' '.$pk.'x'.str_replace(".00","",$sz).'" style="width:310px; id="desc'.$id.'"/></td>';
+						echo '<td><input type="number" step=".01" value="'.htmlspecialchars($qy).'" style="width:50px;" id="qty'.$id.'"/></td>';
+						echo '<td align="right"><input type="number" step=".01" value="'.htmlspecialchars($ret).'"  style="max-width:90px;" id="retail'.$id.'"/></td>';
 						echo '<td align="right"><span id="topr'.$id.'">'.htmlspecialchars($to_pr).'</span></td>';
 						echo '</tr>';
 					}
