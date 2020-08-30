88803cc40add589f7fc11e0506e4452be78a6c31
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-30 15:23:13 -0700

    Update invoice.js

--- js/invoice.js
+++ js/invoice.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
 	var qty = "";
 	var qty_old = "";
 	
+	var retail = "";
+	var retail_old = "";
 	
 	//store the number in current input incase user enter invalid number
 	$('input').focus(function(){
 		var currentId = $(this).attr('id');
 		if (currentId.indexOf("qty") >= 0){
 				qty_old = $(this).val();
-				
-			}
+		}
+		if (currentId.indexOf("retail") >= 0){
+				retail_old = $(this).val();
+		}
 		
 	});
 	
@@ -22,16 +26,31 @@ $(document).ready(function(){
 				id = $(this).attr('id').replace('qty', '');
 				qty = $(this).val();
 				
-			}
-		
-		if(!$.isNumeric(qty)) {
-				$(this).val(qty_old);
-			}
-			else{
-				//$.post('../update-cellphone-goal.php', {dt: dt, store: store, amount: amount}, function(data){
+				if(!$.isNumeric(qty)) {
+					$(this).val(qty_old);
+				}
+				else{
+					//$.post('../update-cellphone-goal.php', {dt: dt, store: store, amount: amount}, function(data){
 				
-				//});
+					//});
+				}
+				
 			}
+		if (currentId.indexOf("retail") >= 0){
+				id = $(this).attr('id').replace('retail', '');
+				retail = $(this).val();
+				if(!$.isNumeric(retail)) {
+					$(this).val(retail_old);
+				}
+				else{
+					//$.post('../update-cellphone-goal.php', {dt: dt, store: store, amount: amount}, function(data){
+				
+					//});
+				}
+				
+		}
+		
+		
 	});
 	
 	
