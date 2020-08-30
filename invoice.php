9102d1d2f93dd3472677337f41f7a51ac81250f3
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-30 14:12:12 -0700

    working on invoice

--- invoice.php
+++ invoice.php
@@ -203,20 +203,20 @@ if(isset($_GET['invoice']) && !empty($_GET['invoice'])){
 		<tbody>
 			<tr>
 				<?php 
-					$query = "SELECT `cert_code`, `qty`, `description`, `retail`, `total_price`, `Pack`, `size` FROM `requested_items` WHERE `clientid` = '$clientid' AND `invoice_number_hash` = ?";
+					$query = "SELECT `id`, `cert_code`, `qty`, `description`, `retail`, `total_price`, `Pack`, `size` FROM `requested_items` WHERE `clientid` = '$clientid' AND `invoice_number_hash` = ?";
 					$stmt = $link->prepare($query);
 					$stmt->bind_param('s', $hashed_invoice_number);
 					$stmt->execute();
-					$stmt->bind_result($ce_co, $qy, $des, $ret, $to_pr, $pk, $sz);
+					$stmt->bind_result($id, $ce_co, $qy, $des, $ret, $to_pr, $pk, $sz);
 					$x = 0;
 					while($stmt->fetch()){
 						$x++;
 						echo '<tr>';
 						echo '<td>'.htmlspecialchars($ce_co).'</td>';
-						echo '<td><input type="text" value="'.htmlspecialchars($des).' '.$pk.'x'.$sz.'" id="desc" style="width:310px;"/></td>';
-						echo '<td><input type="text" value="'.htmlspecialchars($qy).'" id="qty" style="width:50px;"/></td>';
-						echo '<td align="right"><input type="text" value="'.htmlspecialchars($ret).'" id="retail" style="max-width:90px;"/></td>';
-						echo '<td align="right">'.htmlspecialchars($to_pr).'</td>';
+						echo '<td><input type="text" value="'.htmlspecialchars($des).' '.$pk.'x'.$sz.'" id="desc" style="width:310px; id="desc'.$id.'"/></td>';
+						echo '<td><input type="text" value="'.htmlspecialchars($qy).'" id="qty" style="width:50px;" id="qty'.$id.'"/></td>';
+						echo '<td align="right"><input type="text" value="'.htmlspecialchars($ret).'" id="retail" style="max-width:90px;" id="ret'.$id.'"/></td>';
+						echo '<td align="right"><span id="topr'.$id.'">'.htmlspecialchars($to_pr).'</span></td>';
 						echo '</tr>';
 					}
 					if($x < 20){
