57fcc2236230122661be2c297da6f43b7fac6649
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-30 14:44:04 -0700

    working on invoice jquery

--- js/invoice.js
+++ js/invoice.js
@@ -1,29 +1,24 @@
 $(document).ready(function(){
-	
-	$('input[type="text"]').focusout(function() {
-	    $('input[type="text"]').removeClass('blur');
+	var id = "";
+	var qty = "";
+	$('input[type="number"]').focusout(function() {
+	    $('input[type="number"]').removeClass('blur');
 	    $(this).addClass('blur');
 	    
-	    var currentId = $(this).attr('id');
+	    if (str.indexOf("qty") >= 0){
+				id = $(this).attr('id').replace('qty', '');
+				qty = $(this).val();
+				alert(qty);
+			}
 		
-		var dt = currentId.substr(0, 10);
-		var store = currentId.substr(11, 2);
-		var amount = $('#'+currentId).val();
-		
-		
-		if (currentId.length = 13){
-		
-		
-			if(!$.isNumeric(amount)) {
+		if(!$.isNumeric(qyu)) {
 				
 			}
 			else{
-				$.post('../update-cellphone-goal.php', {dt: dt, store: store, amount: amount}, function(data){
+				//$.post('../update-cellphone-goal.php', {dt: dt, store: store, amount: amount}, function(data){
 				
 				});
 			}
-
-		}
 	});
 	
 	
