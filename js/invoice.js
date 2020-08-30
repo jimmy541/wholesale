25283ebf53e2892378c1fda919a859ab41def392
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-30 15:17:25 -0700

    Update invoice.js

--- js/invoice.js
+++ js/invoice.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 	var id = "";
 	var qty = "";
+	var qty_old = "";
+	
+	
+	//store the number in current input incase user enter invalid number
+	$('input').focus(function(){
+		var currentId = $(this).attr('id');
+		if (currentId.indexOf("qty") >= 0){
+				qty_old = $(this).val();
+				
+			}
+		
+	});
+	
 	$('input[type="number"]').focusout(function() {
 	    $('input[type="number"]').removeClass('blur');
 	    $(this).addClass('blur');
@@ -12,7 +25,7 @@ $(document).ready(function(){
 			}
 		
 		if(!$.isNumeric(qty)) {
-				
+				$(this).val(qty_old);
 			}
 			else{
 				//$.post('../update-cellphone-goal.php', {dt: dt, store: store, amount: amount}, function(data){
