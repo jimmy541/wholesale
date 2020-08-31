b0544508ea437198259ee9dcd03d41556d4a8c20
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-08-30 22:13:20 -0700

    Update edit-user.php

--- edit-user.php
+++ edit-user.php
@@ -10,15 +10,17 @@ $first_name = '';
 $last_name = '';
 $display_code = '';
 $email = '';
-$role = '';
+$user_role = '';
+$allow_limited_override = '';
+$allow_free_override = '';
 if(isset($_GET['user']) && !empty($_GET['user'])){
 	$user_code = $_GET['user'];
 	
-	$query="SELECT `email_address`, `first_name`, `last_name`, `display_code`, `role` FROM `users` WHERE `uid` = ? AND `clientid` = ?";
+	$query="SELECT `email_address`, `first_name`, `last_name`, `display_code`, `role`, `allow_price_override`, `allow_free_override` FROM `users` WHERE `uid` = ? AND `clientid` = ?";
 	$stmt = $link->prepare($query);
 	$stmt->bind_param('ss', $user_code, $clientid);
 	$stmt->execute();
-	$stmt->bind_result($em, $fn, $ln, $dc, $ro);
+	$stmt->bind_result($em, $fn, $ln, $dc, $ro, $apo, $afo);
 	 //switch to false when done testing
 	while ($stmt->fetch()) {
 		$found = 'true';
@@ -26,7 +28,10 @@ if(isset($_GET['user']) && !empty($_GET['user'])){
 		$first_name = $fn;
 		$last_name = $ln;
 		$display_code = $dc;
-		$role = $ro;
+		$user_role = $ro;
+		$allow_limited_override = $apo;
+		$allow_price_override = $afo;
+		
 		
 	}
 }
@@ -85,12 +90,33 @@ if(isset($_GET['success']) && $_GET['success'] == 1){$responseMsg = '<span class
 						<div class="col-md-6 mb-3">
 							<label for="">Role:*</label>
 							<select class="form-control"   id="user_role" name="user_role">
-								<option <?php if($role=='Administrator'){echo 'selected';} ?>>Administrator</option>
-								<option <?php if($role=='Backend Operator'){echo 'selected';} ?>>Backend Operator</option>
-								<option <?php if($role=='Sales Representative'){echo 'selected';} ?>>Sales Representative</option>
+								<option <?php if($user_role=='Administrator'){echo 'selected';} ?>>Administrator</option>
+								<option <?php if($user_role=='Backend Operator'){echo 'selected';} ?>>Backend Operator</option>
+								<option <?php if($user_role=='Sales Representative'){echo 'selected';} ?>>Sales Representative</option>
 							</select>
 						</div>
 					</div>
+					<hr class="mb-4">
+			<h4 class="mb-3">Settings</h4>
+			<div class="form-group">
+			<div class="form-check">
+			  <input class="form-check-input" type="checkbox" value="" id="allow_limited_override" name="allow_limited_override" <?php if($allow_limited_override == '1'){echo 'checked';} if($user_role == 'Administrator'){echo 'disabled';}  ?>>
+			  <label class="form-check-label" for="allow_limited_override">
+				Allow Limited Price Override
+			  </label>
+			 
+			</div>
+		  </div>
+		  <div class="form-group">
+			<div class="form-check">
+			  <input class="form-check-input" type="checkbox" value="" id="allow_free_override" name="allow_free_override" <?php if($allow_free_override == '1'){echo 'checked';} if($user_role == 'Administrator'){echo 'disabled';} ?>>
+			  <label class="form-check-label" for="allow_free_override">
+				Allow Free Price Override
+			  </label>
+			 
+			</div>
+		  </div>
+					
 					<hr class="mb-4">
 					<div class="mb-3">
 						<button class="btn btn-primary btn-lg btn-block" type="submit">Save</button>
