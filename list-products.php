adb6f98b14f22ead6ba0d178da3e00caefca3a03
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-09-03 01:52:20 -0700

    Update list-products.php

--- list-products.php
+++ list-products.php
@@ -69,159 +69,167 @@ if(isset($_GET['supplier']) && !empty($_GET['supplier'])){
 
  </p>
 <div class="container-fluid">
-<div class="row collapse" id="filter-box">
-<div class="col">
-	<h4>Filter</h4>
-	<form method="get">
-		<div class="form-row">
-			<div class="col-md-3 mb-3">
-				 <label for="department">Department</label>
-				<select class="form-control" id="department" name="department" style="height: 38px !important">
-					<option></option>
-					<?php
-						$query = "SELECT `id`, `description` FROM `department` WHERE `clientid` = '$clientid' ORDER BY `description` ASC";
-						$stmt = $link->prepare($query);
-						$stmt->execute();
-						$stmt->bind_result($id, $description);
-						while($stmt->fetch()){
-							$selected = '';
-							if($id == $department){$selected = 'selected';}
-							echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($description).'</option>';
-						}
-						$stmt->close();
-					?>
-				</select>
+	<div class="row collapse" id="filter-box">
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
+			<h4>Filter</h4>
+			<form method="get">
+			<div class="form-row">
+				<div class="col-md-3 mb-3">
+					 <label for="department">Department</label>
+					<select class="form-control" id="department" name="department" style="height: 38px !important">
+						<option></option>
+						<?php
+							$query = "SELECT `id`, `description` FROM `department` WHERE `clientid` = '$clientid' ORDER BY `description` ASC";
+							$stmt = $link->prepare($query);
+							$stmt->execute();
+							$stmt->bind_result($id, $description);
+							while($stmt->fetch()){
+								$selected = '';
+								if($id == $department){$selected = 'selected';}
+								echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($description).'</option>';
+							}
+							$stmt->close();
+						?>
+					</select>
+				</div>
+				<div class="col-md-3 mb-3">
+					<label for="subdepartment">Sub Department</label>
+					<select class="form-control" id="subdepartment" name="subdepartment" style="height: 38px !important">
+						<option></option>
+						<?php
+							$query = "SELECT `id`, `description` FROM `sub_department` WHERE `clientid` = '$clientid' ORDER BY `description` ASC";
+							$stmt = $link->prepare($query);
+							$stmt->execute();
+							$stmt->bind_result($id, $description);
+							while($stmt->fetch()){
+								$selected = '';
+								if($id == $subdepartment){$selected = 'selected';}
+								echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($description).'</option>';
+							}
+							$stmt->close();
+						?>
+					</select>
+				</div>
+				<div class="col-md-3 mb-3">
+					<label for="category">Category</label>
+					<select class="form-control" id="category" name="category" style="height: 38px !important">
+						<option></option>
+						<?php
+							$query = "SELECT `id`, `description` FROM `sub_department` WHERE `clientid` = '$clientid' ORDER BY `description` ASC";
+							$stmt = $link->prepare($query);
+							$stmt->execute();
+							$stmt->bind_result($id, $description);
+							while($stmt->fetch()){
+								$selected = '';
+								if($id == $category){$selected = 'selected';}
+								echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($description).'</option>';
+							}
+							$stmt->close();
+						?>
+					</select>
+				</div>
+				<div class="col-md-3 mb-3">
+					<label for="supplier">Supplier</label>
+					<select class="form-control" id="supplier" name="supplier" style="height: 38px !important">
+						<option></option>
+						<?php
+							$query = "SELECT `id`, `name` FROM `supplier` WHERE `clientid` = '$clientid' ORDER BY `name` ASC";
+							$stmt = $link->prepare($query);
+							$stmt->execute();
+							$stmt->bind_result($id, $name);
+							while($stmt->fetch()){
+								$selected = '';
+								if($id == $supplier){$selected = 'selected';}
+								echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($name).'</option>';
+							}
+							$stmt->close();
+						?>
+					</select>
+				</div>
 			</div>
-			<div class="col-md-3 mb-3">
-				<label for="subdepartment">Sub Department</label>
-				<select class="form-control" id="subdepartment" name="subdepartment" style="height: 38px !important">
-					<option></option>
-					<?php
-						$query = "SELECT `id`, `description` FROM `sub_department` WHERE `clientid` = '$clientid' ORDER BY `description` ASC";
-						$stmt = $link->prepare($query);
-						$stmt->execute();
-						$stmt->bind_result($id, $description);
-						while($stmt->fetch()){
-							$selected = '';
-							if($id == $subdepartment){$selected = 'selected';}
-							echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($description).'</option>';
-						}
-						$stmt->close();
-					?>
-				</select>
+			<div class="form-row">
+				<div class="col-md-4 mb-3">
+				
+				</div>
+				<div class="col-md-4 mb-3">
+				
+				</div>
+				<div class="col-md-4 mb-3">
+				
+				</div>
 			</div>
-			<div class="col-md-3 mb-3">
-				<label for="category">Category</label>
-				<select class="form-control" id="category" name="category" style="height: 38px !important">
-					<option></option>
-					<?php
-						$query = "SELECT `id`, `description` FROM `sub_department` WHERE `clientid` = '$clientid' ORDER BY `description` ASC";
-						$stmt = $link->prepare($query);
-						$stmt->execute();
-						$stmt->bind_result($id, $description);
-						while($stmt->fetch()){
-							$selected = '';
-							if($id == $category){$selected = 'selected';}
-							echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($description).'</option>';
-						}
-						$stmt->close();
-					?>
-				</select>
-			</div>
-			<div class="col-md-3 mb-3">
-				<label for="supplier">Supplier</label>
-				<select class="form-control" id="supplier" name="supplier" style="height: 38px !important">
-					<option></option>
-					<?php
-						$query = "SELECT `id`, `name` FROM `supplier` WHERE `clientid` = '$clientid' ORDER BY `name` ASC";
-						$stmt = $link->prepare($query);
-						$stmt->execute();
-						$stmt->bind_result($id, $name);
-						while($stmt->fetch()){
-							$selected = '';
-							if($id == $supplier){$selected = 'selected';}
-							echo '<option value="'.htmlspecialchars($id).'" '.$selected.'>'.htmlspecialchars($name).'</option>';
-						}
-						$stmt->close();
-					?>
-				</select>
+			 <input type="submit" class="btn btn-primary shadow mb-2" id="sbbtn" value="Search" />
+		
+		</form>
 			</div>
 		</div>
-		<div class="form-row">
-			<div class="col-md-4 mb-3">
-			
+	</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-body">
+					<table class="row-border" id="gtable">
+				<thead>
+					
+					<tr>
+						<th>Item</th>
+						<th>Brand</th>
+						<th>Description</th>
+						<th>Size</th>
+						<th>Cost</th>
+						<th>Retail</th>
+						<th>Margin</th>
+						<th>Qty</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tfoot>
+					
+					<tr>
+						<th>Item</th>
+						<th>Brand</th>
+						<th>Description</th>
+						<th>Size</th>
+						<th></th>
+						<th></th>
+						<th></th>
+						<th></th>
+						<th></th>
+					</tr>
+				</tfoot>
+				<tbody>
+					<?php
+					$query = "SELECT a.`uniqueid`, a.`cert_code`, a.`description`, a.`size_amount`, c.`description` brnd, b.`description` wd, a.`case_cost`, a.`case_price`, a.`QtyOnHand` FROM `grocery_products` a LEFT JOIN `weight_units` b ON a.`size_unit` = b.`id` AND b.`clientid` = '$clientid' LEFT JOIN `brands` c ON a.`brand` = c.`id` AND c.`clientid` = '$clientid' WHERE  a.`clientid` = '$clientid' $department_query $subdepartment_query $category_query $supplier_query";
+					
+					$result = mysqli_query($link, $query); 
+					while($row = mysqli_fetch_array($result)) { 
+						echo '<tr><td data-label="item">'.htmlspecialchars($row["cert_code"]).'</td>
+						<td data-label="Brand">'.htmlspecialchars($row["brnd"]).'</td>
+						<td data-label="Description">'.htmlspecialchars($row["description"]).'</td>
+						<td data-label="Size">'.htmlspecialchars(@number_format($row['size_amount'], 1)).' '.htmlspecialchars($row['wd']).'</td>
+						<td data-label="Cost">'.htmlspecialchars($row['case_cost']).'</td>
+						<td data-label="Retail">'.htmlspecialchars($row['case_price']).'</td>';
+						$mrgn = 'NA';
+						if(!empty($row['case_price']) && !empty($row['case_cost'])){
+							if($row['case_price'] > 0){
+								$mrgn = number_format(((($row['case_price']-$row['case_cost']) / $row['case_price']) * 100),2).'%';
+							}
+						}
+						echo '<td data-label="Case Retail">'.$mrgn.'</td>
+						<td data-label="Qty">'.htmlspecialchars($row['QtyOnHand']).'</td>
+						<td><span class="action-icons"><a href="edit-product.php?product='.htmlspecialchars($row['uniqueid']).'"><i class="fas fa-edit"></i></a><i id="delete'.htmlspecialchars($row['uniqueid']).'" class="fas fa-trash-alt"></i></span></td></tr>'; 
+					}
+					?>
+			</tbody>
+			</table>
+				</div>
 			</div>
-			<div class="col-md-4 mb-3">
 			
-			</div>
-			<div class="col-md-4 mb-3">
-			
-			</div>
 		</div>
-		 <input type="submit" class="btn btn-primary shadow mb-2" id="sbbtn" value="Search" />
-    
-	</form>
-</div>
-</div>
-<div class="row">
-<div class="col">
-
-<table class="row-border" id="gtable">
-	<thead>
-		
-		<tr>
-			<th>Item</th>
-			<th>Brand</th>
-			<th>Description</th>
-			<th>Size</th>
-			<th>Cost</th>
-			<th>Retail</th>
-			<th>Margin</th>
-			<th>Qty</th>
-			<th>Action</th>
-		</tr>
-	</thead>
-	<tfoot>
-		
-		<tr>
-			<th>Item</th>
-			<th>Brand</th>
-			<th>Description</th>
-			<th>Size</th>
-			<th></th>
-			<th></th>
-			<th></th>
-			<th></th>
-			<th></th>
-		</tr>
-	</tfoot>
-	<tbody>
-		<?php
-		$query = "SELECT a.`uniqueid`, a.`cert_code`, a.`description`, a.`size_amount`, c.`description` brnd, b.`description` wd, a.`case_cost`, a.`case_price`, a.`QtyOnHand` FROM `grocery_products` a LEFT JOIN `weight_units` b ON a.`size_unit` = b.`id` AND b.`clientid` = '$clientid' LEFT JOIN `brands` c ON a.`brand` = c.`id` AND c.`clientid` = '$clientid' WHERE  a.`clientid` = '$clientid' $department_query $subdepartment_query $category_query $supplier_query";
-		
-		$result = mysqli_query($link, $query); 
-		while($row = mysqli_fetch_array($result)) { 
-			echo '<tr><td data-label="item">'.htmlspecialchars($row["cert_code"]).'</td>
-			<td data-label="Brand">'.htmlspecialchars($row["brnd"]).'</td>
-			<td data-label="Description">'.htmlspecialchars($row["description"]).'</td>
-			<td data-label="Size">'.htmlspecialchars(@number_format($row['size_amount'], 1)).' '.htmlspecialchars($row['wd']).'</td>
-			<td data-label="Cost">'.htmlspecialchars($row['case_cost']).'</td>
-			<td data-label="Retail">'.htmlspecialchars($row['case_price']).'</td>';
-			$mrgn = 'NA';
-			if(!empty($row['case_price']) && !empty($row['case_cost'])){
-				if($row['case_price'] > 0){
-					$mrgn = number_format(((($row['case_price']-$row['case_cost']) / $row['case_price']) * 100),2).'%';
-				}
-			}
-			echo '<td data-label="Case Retail">'.$mrgn.'</td>
-			<td data-label="Qty">'.htmlspecialchars($row['QtyOnHand']).'</td>
-			<td><span class="action-icons"><a href="edit-product.php?product='.htmlspecialchars($row['uniqueid']).'"><i class="fas fa-edit"></i></a><i id="delete'.htmlspecialchars($row['uniqueid']).'" class="fas fa-trash-alt"></i></span></td></tr>'; 
-		}
-		?>
-</tbody>
-</table>
-</div>
-</div>
+	</div>
 </div>
 <script type="text/javascript">
 /*
