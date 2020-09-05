acb050695806c08e826e18fa01f57d8259a7005f
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-09-05 12:16:03 -0700

    Update list-users.php

--- list-users.php
+++ list-users.php
@@ -7,7 +7,7 @@ $more_script = '<script type="text/javascript" src="js/jquery.dataTables.min.js"
 require($_SERVER['DOCUMENT_ROOT'].'/wholesale/include/header.php');
 
 ?>
-<br/>
+<h3 class="page-header"><?php echo $page_title; ?></h3>
 
 <?php
 if(isset($_GET['as'])){
@@ -27,35 +27,50 @@ if(isset($_GET['rs'])){
 }
  ?>
 
-<br/>
-<form action="new-user.php"><button type="submit" class="btn btn-primary shadow btn-lg"">New User</button></form>
-<table class="row-border" id="gtable">
-<caption>Users</caption>
-	<thead>
-		<tr>
-			<th>First Name</th>
-			<th>Last Name</th>
-			<th>Code</th>
-			<th>Role</th>
-			<th>Action</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-		$query = "SELECT * FROM `users` WHERE `clientid` = '$clientid' AND `role` <> 'Owner' ";
-		$result = mysqli_query($link, $query); 
-		while($row = mysqli_fetch_array($result)) { 
-			echo '<tr>
-			<td data-label="First Name">'.htmlspecialchars($row["first_name"]).'</a></td>
-			<td data-label="Last Name">'.htmlspecialchars($row["last_name"]).'</td>
-			<td data-label="Display Code">'.$row['display_code'].'</td>
-			<td data-label="Role">'.$row['role'].'</td>
-			<td><span class="action-icons"><a href="edit-user.php?user='.$row['uid'].'"><i class="fas fa-edit"></i></a><i id="delete'.$row['uid'].'" class="fas fa-trash-alt"></i></span></td>
-			</tr>'; 
-		}
-		?>
-</tbody>
-</table>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<form action="new-user.php"><button type="submit" class="btn btn-primary shadow btn-sm float-right">New User</button></form>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-body">
+					<table class="row-border" id="gtable">
+						<thead>
+							<tr>
+								<th>First Name</th>
+								<th>Last Name</th>
+								<th>Code</th>
+								<th>Role</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+							$query = "SELECT * FROM `users` WHERE `clientid` = '$clientid' AND `role` <> 'Owner' ";
+							$result = mysqli_query($link, $query); 
+							while($row = mysqli_fetch_array($result)) { 
+								echo '<tr>
+								<td data-label="First Name">'.htmlspecialchars($row["first_name"]).'</a></td>
+								<td data-label="Last Name">'.htmlspecialchars($row["last_name"]).'</td>
+								<td data-label="Display Code">'.$row['display_code'].'</td>
+								<td data-label="Role">'.$row['role'].'</td>
+								<td><span class="action-icons"><a href="edit-user.php?user='.$row['uid'].'"><i class="fas fa-edit"></i></a><i id="delete'.$row['uid'].'" class="fas fa-trash-alt"></i></span></td>
+								</tr>'; 
+							}
+							?>
+					</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+</div>
 <script type="text/javascript">
 $(document).ready(function() {
     $('#gtable').DataTable();
