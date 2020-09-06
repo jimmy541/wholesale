f658e8255144075b8b84e973a18366704d23e4c4
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-09-06 16:04:13 -0700

    Update order.php

--- order.php
+++ order.php
@@ -98,7 +98,97 @@ if(isset($_GET['customer']) && !empty($_GET['customer'])){
 			</div>
 		</div>
 		<div class="col-10">
+			<div class="content">
+	<?php
+	if (isset($_GET['acat'])){
+		 
 		
+		if ($_GET['acat'] == 'uncat'){
+			echo '<div class="p-3 mb-2 bg-light text-dark">Uncategorized</div>';
+		}else{
+			echo '<div class="p-3 mb-2 bg-light text-dark">'.getDescription($link, 'department', $clientid, $_GET['acat']).'</div>';
+		}
+		$getid = '';
+		$stmt = $link->prepare("SELECT `id` FROM `department` WHERE `id` = ? AND `clientid` = '$clientid'");
+		$stmt->bind_param('s', $_GET['acat']);
+				$stmt->execute();
+				$stmt->bind_result($dd);
+				while($stmt->fetch()){
+					$getid=htmlspecialchars($dd);
+				}
+				$stmt->close();
+
+		$query = "SELECT DISTINCT `sub_department` FROM `grocery_products` WHERE `department` = '".$getid."' AND `active` = 'yes'";
+		$result = mysqli_query($link, $query);
+		while ($row = mysqli_fetch_array($result)){
+			//List B Categories
+			
+			echo '<h1>'.getDescription($link, 'sub_department', $clientid, $row['sub_department']).'</h1>';
+			
+					echo '<div class="cCatDiv">';
+					//Listing C Categories
+
+						//get quantity inside requested_items table
+						
+						echo '<div class="itemsMainBox">';
+							$query2 = "SELECT a.*, b.`description` brnd, c.`description` wu, d.`qty` FROM `grocery_products` a LEFT JOIN `brands` b ON a.`brand` = b.`id` AND b.`clientid` = '$clientid' LEFT JOIN `weight_units` c ON a.`size_unit` = c.`id` AND c.`clientid` = '$clientid' LEFT JOIN `requested_items` d ON a.`cert_code` = d.`cert_code` AND d.`invoice_number_hash` = '$escaped_order' AND d.`clientid` = '$clientid' WHERE a.`department` = '".$getid."' AND a.`sub_department` = '".$row['sub_department']."' AND a.`clientid` = '$clientid' AND a.`active` = 'yes'";
+							
+							$result2 = mysqli_query($link, $query2);
+									$cnt = 0;
+									$currentSize = 0;
+									$newCat = 'true';
+									$cert_code = '';
+
+								while($row2 = mysqli_fetch_array($result2)){
+									$cnt += 1;
+									echo '<div class="SingleItemBox" id="SingleItemBox'.htmlspecialchars($row2['cert_code']).'">'.htmlspecialchars($row2['brnd']).' '.htmlspecialchars($row2['description']).'
+										<br/>'.htmlspecialchars($row2['cert_code']).' <b>'.htmlspecialchars($row2['upc']).'</b><br/>
+										Retail: <span style="color:green;">'.number_format($row2['case_price'], 2, '.', ',').'</span>
+										Pack: '.@number_format($row2['Pack'], 0).'
+										<div class="Size">'.number_format($row2['size_amount'], 1, '.', ',').' '.htmlspecialchars($row2['wu']).'</div>';
+										$colorStyle = '';
+										
+										if($row2['QtyOnHand'] > 0){
+											$colorStyle = 'background-color:#e5ffe6;';
+										}else{
+											$colorStyle = 'background-color:#ffe5e5;';
+										}
+										echo '<div class="Qty" style="'.$colorStyle.'">'.($row2['QtyOnHand']+$row2['qty']).'</div>';
+										//<div class="MonAvg"></div>
+										echo '<div class="editIcon"><a href="edit-product.php?product='.$row2['uniqueid'].'" target="blank"><img src="images/edit.png"/></a></div>';
+										if (file_exists('pics/'.htmlspecialchars($row2['image-id']))) {
+											echo '<a href="pics/'.htmlspecialchars($row2['image-id']).'" title="" class="thickbox"><img class="tableImg" src="pics/'.htmlspecialchars($row2['image-id']).'"></a>';
+											}
+										
+											echo '<div class="orderNumbersMain" id="OrderDiv'.htmlspecialchars($row2['cert_code']).'"><center><input class="orderButtons" type="button" value="-" id="minus'.$row2['cert_code'].'"/><input class="orderInput" value="'.checkIfOrderPlaced($link, $row2['cert_code'], $clientid, $customer, $order).'" type="hidden" id="qty'.$row2['cert_code'].'"/><input id="plus'.$row2['cert_code'].'" class="orderButtons" type="button" value="+"/></center></div>
+											<div class="greenOrderStatus" id="greenCircle'.$row2['cert_code'].'" '.checkForGreenCircle($link, $row2['cert_code'], $clientid, $customer, $order).'" >'.checkIfOrderPlaced($link, $row2['cert_code'], $clientid, $customer, $order).'</div>
+										</div>&nbsp;';
+										$newCat = 'false';
+										$cert_code = $row2['cert_code'];
+
+									if ($cnt == 4){
+									
+										$cnt = 0;
+										echo '<br/>';
+										
+									}
+									$currentSize = number_format($row2['size_amount'] * 1, 0);
+									
+								}
+
+							echo '</div>';
+
+					echo '</div>';
+		}
+
+	}
+
+	?>
+
+	<!--close content-div -->
+	</div>
+		
+
 		
 		
 		</div>
@@ -107,96 +197,6 @@ if(isset($_GET['customer']) && !empty($_GET['customer'])){
 	<!-- close container-fluid -->
 </div>
 
-
-<?php
-if (isset($_GET['acat'])){
-	 
-	
-	if ($_GET['acat'] == 'uncat'){
-		echo '<div class="p-3 mb-2 bg-light text-dark">Uncategorized</div>';
-	}else{
-		echo '<div class="p-3 mb-2 bg-light text-dark">'.getDescription($link, 'department', $clientid, $_GET['acat']).'</div>';
-	}
-	$getid = '';
-	$stmt = $link->prepare("SELECT `id` FROM `department` WHERE `id` = ? AND `clientid` = '$clientid'");
-	$stmt->bind_param('s', $_GET['acat']);
-			$stmt->execute();
-			$stmt->bind_result($dd);
-			while($stmt->fetch()){
-				$getid=htmlspecialchars($dd);
-			}
-			$stmt->close();
-
-	$query = "SELECT DISTINCT `sub_department` FROM `grocery_products` WHERE `department` = '".$getid."' AND `active` = 'yes'";
-	$result = mysqli_query($link, $query);
-	while ($row = mysqli_fetch_array($result)){
-		//List B Categories
-		
-		echo '<h1>'.getDescription($link, 'sub_department', $clientid, $row['sub_department']).'</h1>';
-		
-				echo '<div class="cCatDiv">';
-				//Listing C Categories
-
-					//get quantity inside requested_items table
-					
-					echo '<div class="itemsMainBox">';
-						$query2 = "SELECT a.*, b.`description` brnd, c.`description` wu, d.`qty` FROM `grocery_products` a LEFT JOIN `brands` b ON a.`brand` = b.`id` AND b.`clientid` = '$clientid' LEFT JOIN `weight_units` c ON a.`size_unit` = c.`id` AND c.`clientid` = '$clientid' LEFT JOIN `requested_items` d ON a.`cert_code` = d.`cert_code` AND d.`invoice_number_hash` = '$escaped_order' AND d.`clientid` = '$clientid' WHERE a.`department` = '".$getid."' AND a.`sub_department` = '".$row['sub_department']."' AND a.`clientid` = '$clientid' AND a.`active` = 'yes'";
-						
-						$result2 = mysqli_query($link, $query2);
-								$cnt = 0;
-								$currentSize = 0;
-								$newCat = 'true';
-								$cert_code = '';
-
-							while($row2 = mysqli_fetch_array($result2)){
-								$cnt += 1;
-								echo '<div class="SingleItemBox" id="SingleItemBox'.htmlspecialchars($row2['cert_code']).'">'.htmlspecialchars($row2['brnd']).' '.htmlspecialchars($row2['description']).'
-									<br/>'.htmlspecialchars($row2['cert_code']).' <b>'.htmlspecialchars($row2['upc']).'</b><br/>
-									Retail: <span style="color:green;">'.number_format($row2['case_price'], 2, '.', ',').'</span>
-									Pack: '.@number_format($row2['Pack'], 0).'
-									<div class="Size">'.number_format($row2['size_amount'], 1, '.', ',').' '.htmlspecialchars($row2['wu']).'</div>';
-									$colorStyle = '';
-									
-									if($row2['QtyOnHand'] > 0){
-										$colorStyle = 'background-color:#e5ffe6;';
-									}else{
-										$colorStyle = 'background-color:#ffe5e5;';
-									}
-									echo '<div class="Qty" style="'.$colorStyle.'">'.($row2['QtyOnHand']+$row2['qty']).'</div>';
-									//<div class="MonAvg"></div>
-									echo '<div class="editIcon"><a href="edit-product.php?product='.$row2['uniqueid'].'" target="blank"><img src="images/edit.png"/></a></div>';
-									if (file_exists('pics/'.htmlspecialchars($row2['image-id']))) {
-										echo '<a href="pics/'.htmlspecialchars($row2['image-id']).'" title="" class="thickbox"><img class="tableImg" src="pics/'.htmlspecialchars($row2['image-id']).'"></a>';
-										}
-									
-										echo '<div class="orderNumbersMain" id="OrderDiv'.htmlspecialchars($row2['cert_code']).'"><center><input class="orderButtons" type="button" value="-" id="minus'.$row2['cert_code'].'"/><input class="orderInput" value="'.checkIfOrderPlaced($link, $row2['cert_code'], $clientid, $customer, $order).'" type="hidden" id="qty'.$row2['cert_code'].'"/><input id="plus'.$row2['cert_code'].'" class="orderButtons" type="button" value="+"/></center></div>
-										<div class="greenOrderStatus" id="greenCircle'.$row2['cert_code'].'" '.checkForGreenCircle($link, $row2['cert_code'], $clientid, $customer, $order).'" >'.checkIfOrderPlaced($link, $row2['cert_code'], $clientid, $customer, $order).'</div>
-								    </div>&nbsp;';
-									$newCat = 'false';
-								  	$cert_code = $row2['cert_code'];
-
-								if ($cnt == 4){
-								
-									$cnt = 0;
-									echo '<br/>';
-									
-								}
-								$currentSize = number_format($row2['size_amount'] * 1, 0);
-								
-							}
-
-						echo '</div>';
-
-				echo '</div>';
-	}
-
-}
-
-?>
-
-<!--close content-div -->
-</div>
-	
 
 
 
