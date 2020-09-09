eb3222b4461739bd28706d39ad26f246d7006857
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-09-08 21:03:47 -0700

    Update list-customers.php

--- list-customers.php
+++ list-customers.php
@@ -58,7 +58,7 @@ if(isset($_GET['active']) && !empty($_GET['active'])){
 				<li id="customer-statement-ll"><i class="fas fa-list-alt"></i>Statement</li>
 				<li id="customer-list-acchistory"><i class="fas fa-list"></i>History</li>
 				<li id="customer-payment-history"><i class="fas fa-money-check-alt"></i>Payments</li>
-				<li id="assign-salesperson-customer"><i class="fas fa-hand-pointer"></i>Assign</li>
+				<li id="assign-salesperson-customer" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-hand-pointer"></i>Assign</li>
 			</ul>
 		</div>
 	</div>
@@ -146,6 +146,25 @@ $(document).ready(function() {
 			</div>
 		</div>
 	</div>
+</div>
+<div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 
 
