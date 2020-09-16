2827e6addcf4b921835d0ab54f7fae1959cb8700
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-09-15 17:30:23 -0700

    Update header.php

--- include/header.php
+++ include/header.php
@@ -77,7 +77,7 @@ if(isset($_SESSION['user'])){
 		if($curPageName != 'show-products.php')	{
 			require($_SERVER['DOCUMENT_ROOT'].'/wholesale/include/sidebar.php');
 		}else{
-			$pushLeftStyle = 'style="margin-left:0px !important; width: calc(100% - 1px) !important; padding:0px !important"';
+			$pushLeftStyle = 'style="margin-left:0px !important;  padding:0px !important"';
 			$navbar_hidden = 'style="display:none !important;"';
 		}
 	}
