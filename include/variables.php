838f49559617767f93e7d8b00df401bcf11f210c
jimmy541 <69735302+jimmy541@users.noreply.github.com>
2020-09-21 01:32:35 -0700

    Update variables.php

--- include/variables.php
+++ include/variables.php
@@ -1,6 +1,5 @@
 <?php
-$products_links = '
-<ul class="nav">
+$products_links = '<ul class="nav">
 	<a class="nav-link" href="list-products.php" title="Products">Products</a>
 	<a class="nav-link" href="list-products-departments.php" title="Departments">Departments</a>
 	<a class="nav-link" href="list-products-sub-departments.php" title="Sub Departments">Sub Departments</a>
@@ -11,14 +10,4 @@ $products_links = '
 	<a class="nav-link" href="list-products-tax-type.php" title="Tax Types">Tax Types</a>
 	<a class="nav-link" href="list-special-batches.php" title="Specials">Specials</a>
 </ul>';
-
-
-	
-
-
-
 ?>
-
-
-
-
